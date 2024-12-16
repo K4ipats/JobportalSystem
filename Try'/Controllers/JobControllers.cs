cb9280c05ecd58376ffd3b43f691d2a70dfc8e0b
K4ipats <rabenkyle@gmail.com>
2024-12-17 04:28:22 +0900

    Added JobControllers for Passing the applicants data to the View

---
+++ Try'/Controllers/JobControllers.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Try_.Controllers
+{
+    public class JobControllers : Controller
+    {
+        private readonly YourDbContext _context;
+
+        public JobControllers(YourDbContext context)
+        {
+            _context = context;
+        }
+
+        // View Applicants for a Specific Job
+        public ActionResult ViewApplicants(int jobId)
+        {
+            var applicants = _context.Applications
+                .Where(a => a.JobId == jobId)
+                .Select(a => new
+                {
+                    a.Applicant.Name,
+                    a.Applicant.Email,
+                    a.Applicant.PhoneNumber,
+                    a.AppliedDate
+                }).ToList();
+
+            if (applicants == null || !applicants.Any())
+            {
+                return HttpNotFound();
+            }
+
+            return View(applicants);
+        }
+    }
+}
